0816de33821fafcf3cc5898de36daf1a58fcdc51
Scott Montford <scottmontford@gmail.com>
2025-07-16 18:04:05 +0100

    V0.09-2

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    // Explicitly tell the React plugin to handle JSX syntax in .js files.
+    // By default, it only processes .jsx and .tsx files.
+    include: "**/*.{jsx,tsx,js,ts}",
+  })],
 })
