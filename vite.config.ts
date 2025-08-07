40161c99107b4940eb444fc4d71093ce2ae21118
Scott Montford <scottmontford@gmail.com>
2025-08-07 18:38:13 +0100

    V1.20.6
    -Android Prep

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name === 'manifest.json') {
+            return 'manifest.json';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 })
